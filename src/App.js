be532515b8dfe6379cfbd0c0ddd59f0a18efca7a
Oladeji <rajioladeji2@gmail.com>
2020-11-07 00:30:44 +0000

    added login and signup pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <Router>
       <>
         <Route exact path="/" component={Landing} />
+        <Route path="/signup" component={SignUp} />
+        <Route path="/login" component={Login} />
       </>
     </Router>
   );
