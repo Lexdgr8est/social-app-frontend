e6938a7e3b6b7c3047eb926a8b4f4548731105d6
Oladeji <rajioladeji2@gmail.com>
2020-11-08 10:21:29 +0000

    added dashboard and confirm password

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
 import SignUp from "./pages/SignUp";
+import ConfirmPassword from "./pages/ConfirmPassword";
+import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 
 function App() {
@@ -11,6 +13,8 @@ function App() {
         <Route exact path="/" component={Landing} />
         <Route path="/signup" component={SignUp} />
         <Route path="/login" component={Login} />
+        <Route path="/dashboard" component={Dashboard} />
+        <Route path="/confirm-password" component={ConfirmPassword} />
       </>
     </Router>
   );
---
+++ src/pages/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Dashboard = () => {
+    return (
+        <section style={{textAlign: 'center'}} className='dashboard'>
+            <h2>Welcome Boys</h2>
+            <span style={{fontSize: '100px'}} >&#128526;</span>
+        </section>
+    )
+}
+
+export default Dashboard
